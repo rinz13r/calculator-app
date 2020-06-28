2ef49bfcd5887f8ac925640b32de3f001e46a549
rinz13r <srinivasan.vijayraghavan@iiitb.org>
2020-06-28 18:59:12 +0530

    Add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM rust:1.44
+
+#WORKDIR /usr/src/calculator-app
+#
+#COPY . .
+#
+#RUN cargo build --release
+#RUN cargo install --path .
+
+#CMD ["/usr/local/cargo/bin/calculator-app"]
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,28 @@ fn test_add() {
     assert_eq!(12, add(8, 4));
 }
 
+use std::io::*;
+
+fn input (text: &'static str) -> String {
+    // let stdin = std::io::stdin ();
+    let mut buffer = String::new ();
+
+    print! ("{}", text);
+    std::io::stdout().flush ().expect ("flush failed");
+    std::io::stdin ().read_line (&mut buffer).expect ("unable to perform read");
+    buffer.pop ();
+    buffer
+}
+
+use std::str::FromStr;
+
 fn main() {
-    add(1, 2);
-    println!("Hello, world!");
+    let a = f64::from_str (&input ("Enter the first number: ")).unwrap_or_default ();
+    let b = f64::from_str (&input ("Enter the second number: ")).unwrap_or_default ();
+    let op = input ("Enter the operation: ");
+
+    match op.as_str () {
+        "+" => (println! ("{}", add (a, b))),
+        _ => (println! ("Unknown operation"))
+    }
 }
